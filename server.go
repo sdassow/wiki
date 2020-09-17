725c793e73e7c21cdbee0d6ce3e637ae1593ca6b
Simon Dassow <simon@fabams.nl>
2020-09-17 18:42:46 +0200

    Serve static data too

--- server.go
+++ server.go
@@ -565,6 +565,7 @@ func (s *Server) initRoutes() {
 	s.router.GET("/debug/stats", s.StatsHandler())
 
 	s.router.ServeFiles("/css/*filepath", rice.MustFindBox("static/css").HTTPBox())
+	s.router.ServeFiles("/data/*filepath", rice.MustFindBox("static/data").HTTPBox())
 	s.router.ServeFiles("/js/*filepath", rice.MustFindBox("static/js").HTTPBox())
 	s.router.ServeFiles("/webfonts/*filepath", rice.MustFindBox("static/webfonts").HTTPBox())
 	s.router.ServeFiles("/f/*filepath", rice.MustFindBox("static/favicon").HTTPBox())
